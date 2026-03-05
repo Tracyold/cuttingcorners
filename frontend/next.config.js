c6cd4226cea287a7a7658f93a1fe524a0f071b4e
Tracyold <tracy.old.gold@gmail.com>
2026-03-05 19:22:29 +0000

    added ecport to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
@@ -9,3 +10,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
