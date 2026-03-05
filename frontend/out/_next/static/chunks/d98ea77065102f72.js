e321cb02b28467fee06dc67337ddd30f8bc2a4d0
Tracyold <tracy.old.gold@gmail.com>
2026-03-05 19:49:28 +0000

    yarn dev to arn start

---
+++ frontend/out/_next/static/chunks/d98ea77065102f72.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/admin", [
+  "static/chunks/8c62ade3161ca1e5.js",
+  "static/chunks/8b43d336610d1086.js",
+  "static/chunks/6136c71a976f2fb5.js",
+  "static/chunks/turbopack-2d433162ce4c16ea.js"
+])
