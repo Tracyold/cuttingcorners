55aca051cd2c18e1e7006330311e202892871249
Tracyold <tracy.old.gold@gmail.com>
2026-03-08 15:53:46 +0000

    revert footer background

--- frontend/components/shared/Footer.tsx
+++ frontend/components/shared/Footer.tsx
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router';
 
 const footerCss = `
 .ccg-footer {
-  background: #d4af37;
+  background: hsla(0, 0%, 0%, 0.69);
   border-top: 1px solid rgba(255,255,255,0.08);
   padding: 40px 48px;
 }
