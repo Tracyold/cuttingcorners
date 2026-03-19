6be86127c68748c620d0a7a81646b4477938b930
Tracyold <tracy.old.gold@gmail.com>
2026-03-19 19:08:43 +0000

    refactor: remove Google Fonts, use local fonts only

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,12 +4,6 @@ export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oranienbaum&family=Montserrat:wght@300;400;500;600;700;900&family=Lora:ital,wght@0,400;0,500;1,400;1,500&family=DM+Sans:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </Head>
       <body>
         <script dangerouslySetInnerHTML={{ __html: `
