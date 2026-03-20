5768959539ed71bfb3ff03ccda80827d4cb559f5
Tracyold <tracy.old.gold@gmail.com>
2026-03-20 08:57:50 +0000

    fix: use fixed div for background instead of background-attachment fixed

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
   return <>
+    <div style={{ position: 'fixed', inset: 0, zIndex: -1, background: 'var(--bg-gradient)', transition: 'background 600ms cubic-bezier(0.4,0,0.2,1)' }} />
     <Component {...pageProps} />
     <Analytics />
     <SpeedInsights />
