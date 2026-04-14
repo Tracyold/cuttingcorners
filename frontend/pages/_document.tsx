4d327d615a12c74e4e769bef086281d930ccfe6e
Tracyold <tracy.old.gold@gmail.com>
2026-04-14 22:59:56 +0000

    update

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en" data-theme="dark">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
       <body>
         <script dangerouslySetInnerHTML={{ __html: `
